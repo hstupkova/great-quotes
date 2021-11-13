4c9ab482d9a445c7644e85fec1705fd9d59f5fe0
Hana Stupková <hstupkova@gmail.com>
2021-11-13 09:44:53 +0100

    layout styling

--- src/App.js
+++ src/App.js
@@ -3,24 +3,27 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import Quotes from './pages/Quotes';
 import QuoteDetail from './pages/QuoteDetail';
 import NewQuote from './pages/NewQuote';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path="/quotes" exact>
-          <Quotes />
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetail />
-        </Route>
-        <Route path="/new-quote">
-          <NewQuote />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <Quotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
