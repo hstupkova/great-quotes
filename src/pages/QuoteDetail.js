3ad10a3ca6d7c519877d07b53d686a70d2a1a1ed
Hana Stupková <hstupkova@gmail.com>
2021-11-13 16:42:31 +0100

    nested routes to display/show button or comments

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,26 +1,33 @@
 import { Fragment } from 'react';
-import { Route, useParams } from 'react-router-dom';
+import { Route, useParams, Link } from 'react-router-dom';
 
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 
 const DUMMY_QUOTES = [
-  { id: 'q1', author: 'Max', text: 'Learning React is fun!'},
-  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!'}
-]
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
-  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
   if (!quote) {
-    return <p>No quote found.</p>
+    return <p>No quote found.</p>;
   }
 
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
