32b19e66b3ed46908cd20b9deb42af2cd72c3c7a
Hana Stupková <hstupkova@gmail.com>
2021-11-13 09:33:02 +0100

    setting routes, nested routes and redirects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import Quotes from './pages/Quotes';
+import QuoteDetail from './pages/QuoteDetail';
+import NewQuote from './pages/NewQuote';
+
 function App() {
   return (
     <div>
-
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <Quotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
     </div>
   );
 }
