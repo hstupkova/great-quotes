e59993e8691ce34f56fa649cd423ad3920ec9126
Hana Stupková <hstupkova@gmail.com>
2021-11-13 17:26:57 +0100

    fetching data from firebase

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,16 +1,27 @@
-import QuoteForm from '../components/quotes/QuoteForm';
 import { useHistory } from 'react-router-dom';
+import { useEffect } from 'react';
+
+import QuoteForm from '../components/quotes/QuoteForm';
+import useHttp from '../hooks/use-http';
+import { addQuote } from '../lib/api';
 
 const NewQuote = () => {
   const history = useHistory();
+  const { sendRequest, status } = useHttp(addQuote);
+
+  useEffect(() => {
+    if (status === 'completed') {
+      history.push('/quotes');
+    }
+  }, [status, history]);
 
   const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
+    sendRequest(quoteData);
+  };
 
-    history.push('/quotes');
-  }
-
-  return <QuoteForm onAddQuote={addQuoteHandler} />
+  return (
+    <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler} />
+  );
 };
 
 export default NewQuote;
